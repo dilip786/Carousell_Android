5a1b2ac87a14730415cf2d079f494e9efecf90b3
Dilip Kumar <dilip@locusalpha.com>
2021-08-11 01:56:06 +0530

    Init

--- app/src/main/java/com/android/carousell/MyApplication.kt
+++ app/src/main/java/com/android/carousell/MyApplication.kt
@@ -3,13 +3,14 @@ package com.android.carousell
 import android.app.Application
 import android.content.Context
 
-class MyApplication  : Application() {
+class MyApplication : Application() {
     init {
         instance = this
     }
+
     companion object {
         private var instance: MyApplication? = null
-        fun applicationContext() : Context {
+        fun applicationContext(): Context {
             return instance!!.applicationContext
         }
     }
